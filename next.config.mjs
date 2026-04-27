354c352e38da15b22cfcf6cbdfa1fd2b4b67fd15
erickvanreenen <erickvanreenen@gmail.com>
2026-04-27 09:28:59 +0200

    Link redirect

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "v0-product-designer-portfolio-copy3.vercel.app" }],
+        destination: "https://erickvanreenen.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
